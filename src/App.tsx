435487c6ce957935bf23ce30602258c91f563a11
Wanten <WantenMN@gmail.com>
2026-05-05 16:01:45 +0800

    feat: add tailwindcss support

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
-    <main className="container">
-      <h1>Farmuse</h1>
+    <main className="container mx-auto p-4 text-center">
+      <h1 className="text-3xl font-bold underline">Farmuse</h1>
     </main>
   );
 }
